4423f731bffe751e2b8807a19413541c23f93ee9
Stuart Shields <stuart.shields@xwp.co>
2017-01-26 11:09:42 +1000

    Change the way config files are added

--- modules/vip/manifests/init.pp
+++ modules/vip/manifests/init.pp
@@ -2,11 +2,6 @@ class vip (
 	$php = $vip_config[php],
 	$path	= "/vagrant/extensions/vip",
 ) {
-  if ! ( File['/vagrant/content22'] ) {
-    file { "/vagrant/content22":
-      ensure => "directory",
-    }
-  }
 	file { "${$path}/local-config.php":
 		content => template('vip/local-config.php.erb'),
 		owner   => 'root',
@@ -21,24 +16,50 @@ class vip (
 		mode    => 0644,
 	}
 
-	if ! ( File["${$path}/config"] ) {
-		file { "${$path}/config":
-			ensure => "directory",
-			owner   => 'root',
-			group   => 'root',
-			mode    => 0644,
-		}
+	file { "/vagrant/content/config":
+		ensure => "directory",
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0644,
 	}
 
-	file { "${$path}/config/roles.php":
+	file { "/vagrant/content/config/roles.php":
 		content => template('vip/roles.php.erb'),
 		owner   => 'root',
 		group   => 'root',
 		mode    => 0644,
 	}
 
-	file { "${$path}/config/vip-config.php":
+	file { "/vagrant/content/config/vip-config.php":
 		content => template('vip/vip-config.php.erb'),
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0644,
+	}
+
+	file { "/vagrant/content/config/batcache-config.php":
+		content => template('vip/batcache-config.php.erb'),
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0644,
+	}
+
+	file { "/vagrant/content/config/is-mobile.php":
+		content => template('vip/is-mobile.php.erb'),
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0644,
+	}
+
+	file { "/vagrant/content/object-cache.php":
+		content => template('vip/object-cache.php.erb'),
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0644,
+	}
+
+	file { "/vagrant/content/advanced-cache.php":
+		content => template('vip/advanced-cache.php.erb'),
 		owner   => 'root',
 		group   => 'root',
 		mode    => 0644,
